b1ef776f58b96f90ff8ce772b5ceafe89093dd1c
Zulfaquar Haque <ayaanhaque07@gmail.com>
2024-01-18 22:26:17 +0530

    "Dark mode started"

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,48 @@ import { AllUser, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdatePr
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import CreatePost from './_root/pages/CreatePost';
+import { useEffect, useState } from 'react';
 
 
 
 const App = () => {
+  const [dark,setDark]=useState<boolean>(false)
+  // const [theme,setTheme]=useState<boolean>(false);
+    const handleTheme=()=>{
+      // setTheme((prev)=>!prev);
+    const isDark=localStorage.getItem('isDark')??null;
+
+      localStorage.setItem("isDark",isDark==="true"?"false":"true");
+      setDark(isDark==="true"?true:false)
+      if(isDark&&isDark==="true"){
+       
+        document.body.classList.remove('dark-mode')
+        document.body.classList.add('light-mode')
+
+      }else{
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode')
+
+      }
+    }
+
+    const isDark=localStorage.getItem('isDark')??null;
+    useEffect(()=>{
+      if(!isDark){
+        localStorage.setItem("isDark","false");
+      }
+      if(isDark&&Boolean(isDark)){
+        document.body.classList.remove('dark-mode')
+
+      }else{
+        document.body.classList.add('dark-mode')
+
+      }
+
+    },[isDark])
   return (
-    <main className="file h-screen">
+    <main className={`file h-screen `}>
+      <button onClick={handleTheme} className='absolute top-2 right-4 p-2 rounded-xl bg-indigo-500 text-white'>Dark Mode</button>
       <Routes>
         {/* public routes */}
         <Route element={<AuthLayout />}>
@@ -23,7 +59,7 @@ const App = () => {
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
         {/* private routes*/}
-        <Route element={<RootLayout />}>
+        <Route element={<RootLayout dark={dark}/>}>
           <Route index element={<Home />}/>
           <Route path="/explore" element={<Explore/>}/>
           <Route path="/saved" element={<Saved/>}/>
